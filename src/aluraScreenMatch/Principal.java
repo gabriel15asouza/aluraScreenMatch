7b88c7192de9c0509d30bf14ffabaedf91c99677
gabriel15asouza <gabalves.souza@gmail.com>
2023-09-01 15:25:23 -0300

    initial implementation of enpsulation
    Making some attributes private

--- src/aluraScreenMatch/Principal.java
+++ src/aluraScreenMatch/Principal.java
@@ -7,16 +7,12 @@ public class Principal {
 		filme1.anoDeLancamento = 1994;
 		filme1.duracaoEmMinutos = 144;
 		filme1.incluidoNoPlano = true;
-		filme1.somaDasAvaliacoes = 0;
-		filme1.totalDeAvaliacoes = 0;
 		
 		filme1.exibeFichaTecnica();
 		filme1.avalia(9);
 		filme1.avalia(10);
 		filme1.avalia(10);
 		
-		System.out.println(filme1.somaDasAvaliacoes);
-		System.out.println(filme1.totalDeAvaliacoes);
 		System.out.println(filme1.pegaMedia());
 		}
 }
